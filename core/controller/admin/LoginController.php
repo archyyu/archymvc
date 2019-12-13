94dd1a481f5d07b8ec50e6b9823b8d0459c3e0e5
yujiangpeng <archy_yu@sina.com>
2019-12-13 09:49:29 +0800

    cm

---
+++ core/controller/admin/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2019/12/12
+ * Time: 16:54
+ */
+
+namespace controller\admin;
+
+
+use controller\Controller;
+
+class LoginController extends Controller
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index(){
+
+//        $this->smarty
+
+    }
+
+    public function login(){
+
+    }
+
+}
